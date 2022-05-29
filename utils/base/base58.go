4d129d7a1c7ccc03350e25e302cbc2aaa2baf592
me <cpw0507@163.com>
2022-05-29 17:46:21 +0800

    modify base58

--- utils/base/base58.go
+++ utils/base/base58.go
@@ -41,12 +41,27 @@ func reverseByteArr(bytes []byte) []byte {
 }
 
 // Base58Decode Base58解码
-func Base58Decode(str []byte) []byte {
-	ret := big.NewInt(0)
-	for _, byteElem := range str {
-		index := bytes.IndexByte(base58, byteElem) // 获取base58对应数组的下标
-		ret.Mul(ret, big.NewInt(58))               // 相乘
-		ret.Add(ret, big.NewInt(int64(index)))     // 相加
+func Base58Decode(input []byte) []byte {
+	result := big.NewInt(0)
+	zeroBytes := 0
+	for _, b := range input {
+		if b == '1' {
+			zeroBytes++
+		} else {
+			break
+		}
 	}
-	return ret.Bytes()
+
+	payload := input[zeroBytes:]
+
+	for _, b := range payload {
+		charIndex := bytes.IndexByte(base58, b)          // 反推出余数
+		result.Mul(result, big.NewInt(58))               // 之前的结果乘以58
+		result.Add(result, big.NewInt(int64(charIndex))) // 加上这个余数
+
+	}
+
+	decoded := result.Bytes()
+	decoded = append(bytes.Repeat([]byte{0x00}, zeroBytes), decoded...)
+	return decoded
 }
