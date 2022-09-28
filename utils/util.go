d1e467480c6f3cce519329cb139c349d8acda678
robert031 <robert031@163.com>
2022-09-28 13:56:32 +0800

    add HexStringToBytes

--- utils/util.go
+++ utils/util.go
@@ -1,5 +1,7 @@
 package utils
 
+import "strings"
+
 // RemoveDuplicateElement 数组去重
 func RemoveDuplicateElement(array []string) []string {
 	result := make([]string, 0, len(array))
@@ -11,4 +13,26 @@ func RemoveDuplicateElement(array []string) []string {
 		}
 	}
 	return result
+}
+
+func charToByte(c byte) byte {
+	return byte(strings.IndexByte("0123456789ABCDEF", c))
+}
+
+func HexStringToBytes(hexString string) []byte {
+	if hexString == "" || len(hexString)%2 != 0 {
+		return nil
+	}
+
+	hexString = strings.ToUpper(hexString)
+	length := len(hexString) / 2
+
+	d := make([]byte, length)
+
+	for i := 0; i < length; i++ {
+		pos := i * 2
+		d[i] = charToByte(hexString[pos])<<4 | charToByte(hexString[pos+1])
+	}
+
+	return d
 }
